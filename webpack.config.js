a4b43e267651f5b25e4e4d8857c944042d17fb23
Cody Moore <codymoore240@gmail.com>
2018-11-29 21:42:30 -0500

    Implemented tests

--- webpack.config.js
+++ webpack.config.js
@@ -15,13 +15,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/react'],
-          plugins: [
-            "@babel/plugin-proposal-class-properties"
-          ]
-        },
+        loader: 'babel-loader'
       },
       {
         test: /\.scss$/,
