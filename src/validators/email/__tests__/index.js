c346e3648fe5abf877c246a13325453b98eda503
Cody Moore <codymoore240@gmail.com>
2018-12-04 13:42:41 -0500

    Wrote tests for email validator

--- src/validators/email/__tests__/index.js
+++ src/validators/email/__tests__/index.js
@@ -1,4 +1,32 @@
 import emailRegex from '../regex'
+import emailValidator from '..'
+
+describe('emailValidator', () => {
+  it('returns the correct payload when given a valid email', () => {
+    const email = 'validEmail@domain.com'
+    expect(emailValidator(email)).toEqual({
+      valid: true,
+      message: null,
+      value: 'validemail@domain.com'
+    })
+  }),
+  it('returns the correct payload when given an invalid email', () => {
+    const email = 'invalid'
+    expect(emailValidator(email)).toEqual({
+      valid: false,
+      message: `Invalid format`,
+      value: null
+    })
+  }),
+  it('returns the correct payload when given a blank email', () => {
+    const email = ''
+    expect(emailValidator(email)).toEqual({
+      valid: false,
+      message: `Cannot be blank`,
+      value: null
+    })
+  })
+})
 
 describe('emailRegex', () => {
   it('returns true when given a valid email', () => {
