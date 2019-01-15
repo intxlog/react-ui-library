fd001893bc4f20568e4c915b46d91e7f7b057f88
Keith Moore <kmoore@intxlog.com>
2019-01-15 14:10:05 -0500

    added test coverage for zip in validators

--- src/validators/__tests__/index.js
+++ src/validators/__tests__/index.js
@@ -21,4 +21,14 @@ describe('validators root', () => {
       value: 'any@email.com'
     })
   })
+  it('the zip function is called when zip is the type passed in', () => {
+    const value = 12345
+    const type = 'zip'
+    
+    expect(validator(value,type)).toEqual({
+      valid: true,
+      message: null,
+      value: 12345
+    })
+  })
 })
