d61688658d2f862095460e7356d91fc1c0f6153f
Cody Moore <CodyMoore240@users.noreply.github.com>
2019-02-20 13:55:02 -0500

    Styled the things (#84)
    * Updated packages and styled components to a hard height
    
    * Updated packages, Fixes #6, inputs and selects have a hard height
    
    * WIP- Adding SVGs to modal in place of icons
    
    * Fixes #77. Made icons svg.

--- src/lib/Modal/index.js
+++ src/lib/Modal/index.js
@@ -17,6 +17,7 @@ class Modal extends React.Component {
     let buttonText = null
     let text = null
     let className = null
+    let svg = null
 
     
     //if customContent is passed in then ignore all the rules and just make the content the custom content
@@ -29,16 +30,20 @@ class Modal extends React.Component {
           buttonText = `Ok`
           text = `Success!`
           className = styles.success
+          svg = <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="green" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
           break
         case `warning`:
           buttonText = `Ok`
           text = `Warning!`
           className = styles.warning
+          svg = <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="orange" d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80 35.888-80 80-80 80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z"></path></svg>
+          break
           break
         case `error`:
           buttonText = `Ok`
           text = `Error!`
           className = styles.error
+          svg = <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" ><path fill="red" d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"></path></svg>
           break
       }
   
@@ -60,7 +65,8 @@ class Modal extends React.Component {
   
       content = <>
         <div className={circleClassNames}>
-          <div className={styles.circleContent}></div>
+          {svg}
+          {/* <div className={styles.circleContent}>{svg}</div> */}
         </div>
         <p className={styles.text}>{text}</p>
         <Button
@@ -75,7 +81,7 @@ class Modal extends React.Component {
     if (this.props.isOpen) {
       modal = <div className={styles.container}>
         <div className={styles.modal}>
-          <button className={styles.closeButton} onClick={this.props.closeFunc}></button>
+          <button className={styles.closeButton} onClick={this.props.closeFunc}><svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" ><path fill="white" d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"></path></svg></button>
           {content}
         </div>
       </div>
