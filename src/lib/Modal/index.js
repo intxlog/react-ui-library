0729e7487b3cea8241b9b868e5b528ddf36fb4bc
Cody Moore <codymoore240@gmail.com>
2019-02-20 13:10:58 -0500

    Fixes #77. Made icons svg.

--- src/lib/Modal/index.js
+++ src/lib/Modal/index.js
@@ -30,11 +30,14 @@ class Modal extends React.Component {
           buttonText = `Ok`
           text = `Success!`
           className = styles.success
+          svg = <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="green" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
           break
         case `warning`:
           buttonText = `Ok`
           text = `Warning!`
           className = styles.warning
+          svg = <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512"><path fill="orange" d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80 35.888-80 80-80 80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z"></path></svg>
+          break
           break
         case `error`:
           buttonText = `Ok`
