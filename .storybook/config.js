0a0d9837cee04eede73941ca86cf0fbe37902e62
Cody Moore <codymoore240@gmail.com>
2019-03-07 13:48:26 -0500

    Got storybook working with custom webpack

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,19 @@
-import { configure } from '@storybook/react';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import './styles/base.css'
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+//import stories from the src folder
+const req = require.context('../src', true, /\_story\.js$/)
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach((filename) => req(filename))
 }
+
+//add the info addon 
+addDecorator(
+  withInfo({
+    inline: true
+  })
+)
 
 configure(loadStories, module);
