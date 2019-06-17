02b36d21785d3969a32f7d374ce91a80410e0633
Cody Moore <codymoore240@gmail.com>
2019-06-17 10:59:10 -0400

    Added css loaded to storybook webpack build file

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,6 +2,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.css$/,
+        loaders: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[name]__[local]___[hash:base64:5]',
+            }
+          }
+        ]
+      },
+      {
         test: /\.scss$/,
         loaders: [
           require.resolve('style-loader'),
